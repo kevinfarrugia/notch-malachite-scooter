e33505d64b50858b8695f38cdeef9b4d74dec25b
Glitch (glitch-blank-node) <none>
2022-10-21 08:28:57 +0000

    🗃🍴 Checkpoint ./public/style.css:50063782/688 ./server.js:50063782/976 ./src/pages/index.hbs:50063782/32339

--- server.js
+++ server.js
@@ -1,47 +1,34 @@
 const path = require("path");
 
-// Require the fastify framework and instantiate it
+const getTime = (date) =>
+  `${date.getHours().toString().padStart(2, "0")}:${date
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}`;
+
 const fastify = require("fastify")({
-  // set this to true for detailed logging:
   logger: false,
 });
 
-// Setup our static files
 fastify.register(require("@fastify/static"), {
   root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
+  prefix: "/",
 });
 
-// fastify-formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
-
-// point-of-view is a templating manager for fastify
 fastify.register(require("@fastify/view"), {
   engine: {
     handlebars: require("handlebars"),
   },
 });
 
-// Our main GET home page route, pulls from src/pages/index.hbs
 fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
   let params = {
     greeting: "Hello Node!",
   };
-  // request.query.paramName <-- a querystring example
+
   return reply.view("/src/pages/index.hbs", params);
 });
 
-// A POST route to handle form submissions
-fastify.post("/", function (request, reply) {
-  let params = {
-    greeting: "Hello Form!",
-  };
-  // request.body.paramName <-- a form post example
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-// Run the server and report out to the logs
 fastify.listen(
   { port: process.env.PORT, host: "0.0.0.0" },
   function (err, address) {
