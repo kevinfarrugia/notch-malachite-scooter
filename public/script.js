367ce2a0404386d1329683bb5ff68dc6239adf5b
Glitch (glitch-blank-node) <none>
2022-12-22 12:00:21 +0000

    🛣⛴ Checkpoint ./src/pages/2.hbs:50063782/194 ./src/pages/1.hbs:50063782/366 ./public/script.js:50063782/2447 ./src/pages/3.hbs:50063782/141 ./src/pages/4.hbs:50063782/122 ./src/pages/5.hbs:50063782/122 ./src/pages/6.hbs:50063782/122 ./src/pages/7.hbs:50063782/122 ./src/pages/8.hbs:50063782/122 ./public/style.css:50063782/139

--- public/script.js
+++ public/script.js
@@ -1,15 +1,22 @@
 (function () {
   function getImageDetail(image) {
     if (image) {
-      const { naturalWidth, naturalHeight, clientWidth, clientHeight } = image;
+      const {
+        naturalWidth,
+        naturalHeight,
+        clientWidth,
+        clientHeight,
+        currentSrc,
+      } = image;
 
+      document.getElementById("currentSrc").innerHTML = `${currentSrc.split("/").pop()}`;
       document.getElementById("naturalWidth").innerHTML = `${naturalWidth}px`;
       document.getElementById("naturalHeight").innerHTML = `${naturalHeight}px`;
       document.getElementById("clientWidth").innerHTML = `${clientWidth}px`;
       document.getElementById("clientHeight").innerHTML = `${clientHeight}px`;
 
       const usedPercentage = Math.round(
-        ((clientWidth * clientHeight * dpr) / (naturalWidth * naturalHeight)) *
+        ((clientWidth * clientHeight) / (naturalWidth * naturalHeight)) *
           100
       );
       document.getElementById(
