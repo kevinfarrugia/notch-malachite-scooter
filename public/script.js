c39e3147614dbca776363f8ec6d29d3fdf24c6e2
Glitch (glitch-blank-node) <none>
2022-12-22 12:23:23 +0000

    🌜🚉 Checkpoint ./public/script.js:50063782/21916

--- public/script.js
+++ public/script.js
@@ -1,29 +1,46 @@
 (function () {
   function getImageDetail(image) {
+    document.getElementById("naturalWidth").innerHTML = "-";
+    document.getElementById("naturalHeight").innerHTML = "-";
+    document.getElementById("clientWidth").innerHTML = "-";
+    document.getElementById("clientHeight").innerHTML = "-";
+    document.getElementById("usedPercentage").innerHTML = "-";
+
     if (image) {
-      const {
-        naturalWidth,
-        naturalHeight,
-        clientWidth,
-        clientHeight,
-        currentSrc,
-      } = image;
+      const { clientWidth, clientHeight, currentSrc } = image;
 
-      document.getElementById("currentSrc").innerHTML = `${currentSrc.split("/").pop()}`;
-      document.getElementById("naturalWidth").innerHTML = `${naturalWidth}px`;
-      document.getElementById("naturalHeight").innerHTML = `${naturalHeight}px`;
-      document.getElementById("clientWidth").innerHTML = `${clientWidth}px`;
-      document.getElementById("clientHeight").innerHTML = `${clientHeight}px`;
+      // we download the image again but do not place it in the DOM to get the natural width/height
+      // read more: https://html.spec.whatwg.org/multipage/images.html#normalizing-the-source-densities
+      const offscreenImage = new Image();
+      offscreenImage.src = currentSrc;
+      offscreenImage.onload = () => {
+        const { naturalWidth, naturalHeight } = offscreenImage;
 
-      const usedPercentage = Math.round(
-        ((clientWidth * clientHeight) / (naturalWidth * naturalHeight)) *
-          100
-      );
-      document.getElementById(
-        "usedPercentage"
-      ).innerHTML = `${usedPercentage}%`;
+        document.getElementById("currentSrc").innerHTML = `${currentSrc
+          .split("/")
+          .pop()}`;
+        document.getElementById("naturalWidth").innerHTML = `${naturalWidth}px`;
+        document.getElementById(
+          "naturalHeight"
+        ).innerHTML = `${naturalHeight}px`;
+        document.getElementById("clientWidth").innerHTML = `${clientWidth}px`;
+        document.getElementById("clientHeight").innerHTML = `${clientHeight}px`;
+
+        const usedPercentage = Math.round(
+          ((clientWidth * clientHeight) * Math.pow(dpr, 2) / (naturalWidth * naturalHeight)) * 100
+        );
+        document.getElementById(
+          "usedPercentage"
+        ).innerHTML = `${usedPercentage}%`;
+      };
     }
   }
+
+  document.getElementById("naturalWidth").innerHTML = "-";
+  document.getElementById("naturalHeight").innerHTML = "-";
+  document.getElementById("clientWidth").innerHTML = "-";
+  document.getElementById("clientHeight").innerHTML = "-";
+  document.getElementById("usedPercentage").innerHTML = "-";
 
   const dpr = window.devicePixelRatio;
   document.getElementById("dpr").innerHTML = `${dpr}`;
