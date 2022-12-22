742a70c4434d77af25fbf2347af2c127457477a0
Glitch (glitch-blank-node) <none>
2022-12-22 11:25:25 +0000

    🕋🌷 Checkpoint ./src/pages/1.hbs:50063782/11678 ./public/script.js:50063782/3161

--- public/script.js
+++ public/script.js
@@ -1,12 +1,25 @@
-(function() {
+(function () {
+  function getImageDetail(image) {
+    const { naturalWidth, naturalHeight, clientWidth, clientHeight } = image;
+
+    document.getElementById("naturalWidth").innerHTML = `${naturalWidth}px`;
+    document.getElementById("naturalHeight").innerHTML = `${naturalHeight}px`;
+    document.getElementById("clientWidth").innerHTML = `${clientWidth}px`;
+    document.getElementById("clientHeight").innerHTML = `${clientHeight}px`;
+
+    const unusedPercentage = Math.round(
+      ((clientWidth * clientHeight) / (naturalWidth * naturalHeight)) * 100
+    );
+    document.getElementById(
+      "unusedPercentage"
+    ).innerHTML = `${unusedPercentage}%`;
+  }
+
   const imageEl = document.getElementById("image");
-  const { naturalWidth, naturalHeight, clientWidth, clientHeight} = imageEl;
   
-  document.getElementById("naturalWidth").innerHTML = `${naturalWidth}px`;
-  document.getElementById("naturalHeight").innerHTML = `${naturalHeight}px`;
-  document.getElementById("clientWidth").innerHTML = `${clientWidth}px`;
-  document.getElementById("clientHeight").innerHTML = `${clientHeight}px`;
+  // update on load
+  imageEl.onload = () => getImageDetail(imageEl);
   
-  const unusedPercentage = Math.round((naturalWidth * naturalHeight) / (clientWidth * clientHeight) * 100);
-  document.getElementById("unusedPercentage").innerHTML = `${unusedPercentage}%`;
+  // update on resize
+  window.addEventListener("resize", () => getImageDetail(imageEl));
 })();
