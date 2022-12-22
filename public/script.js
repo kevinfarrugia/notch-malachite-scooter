d716a15bb4a5f147305e1376bad89f2f2f1da9bb
Glitch (glitch-blank-node) <none>
2022-12-22 13:32:51 +0000

    🎯🍿 Checkpoint ./public/script.js:50063782/31235

--- public/script.js
+++ public/script.js
@@ -1,4 +1,50 @@
 (function () {
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+      var context = this,
+        args = arguments;
+      var later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  function updateUI(image) {
+    console.log("j");
+    const { clientWidth, clientHeight, currentSrc } = image;
+
+    // we download the image but do not place it in the DOM to get the natural width/height
+    // read more: https://html.spec.whatwg.org/multipage/images.html#normalizing-the-source-densities
+    const offscreenImage = new Image();
+    offscreenImage.onload = () => {
+      const { naturalWidth, naturalHeight } = offscreenImage;
+
+      document.getElementById("currentSrc").innerHTML = `${currentSrc
+        .split("/")
+        .pop()}`;
+      document.getElementById("naturalWidth").innerHTML = `${naturalWidth}px`;
+      document.getElementById("naturalHeight").innerHTML = `${naturalHeight}px`;
+      document.getElementById("clientWidth").innerHTML = `${clientWidth}px`;
+      document.getElementById("clientHeight").innerHTML = `${clientHeight}px`;
+
+      const usedPercentage = Math.round(
+        ((clientWidth * clientHeight * Math.pow(dpr, 2)) /
+          (naturalWidth * naturalHeight)) *
+          100
+      );
+      document.getElementById(
+        "usedPercentage"
+      ).innerHTML = `${usedPercentage}%`;
+    };
+    offscreenImage.src = currentSrc;
+  }
+
   function getImageDetail(image) {
     document.getElementById("naturalWidth").innerHTML = "-";
     document.getElementById("naturalHeight").innerHTML = "-";
@@ -6,34 +52,7 @@
     document.getElementById("clientHeight").innerHTML = "-";
     document.getElementById("usedPercentage").innerHTML = "-";
 
-    if (image) {
-      const { clientWidth, clientHeight, currentSrc } = image;
-
-      // we download the image again but do not place it in the DOM to get the natural width/height
-      // read more: https://html.spec.whatwg.org/multipage/images.html#normalizing-the-source-densities
-      const offscreenImage = new Image();
-      offscreenImage.src = currentSrc;
-      offscreenImage.onload = () => {
-        const { naturalWidth, naturalHeight } = offscreenImage;
-
-        document.getElementById("currentSrc").innerHTML = `${currentSrc
-          .split("/")
-          .pop()}`;
-        document.getElementById("naturalWidth").innerHTML = `${naturalWidth}px`;
-        document.getElementById(
-          "naturalHeight"
-        ).innerHTML = `${naturalHeight}px`;
-        document.getElementById("clientWidth").innerHTML = `${clientWidth}px`;
-        document.getElementById("clientHeight").innerHTML = `${clientHeight}px`;
-
-        const usedPercentage = Math.round(
-          ((clientWidth * clientHeight) * Math.pow(dpr, 2) / (naturalWidth * naturalHeight)) * 100
-        );
-        document.getElementById(
-          "usedPercentage"
-        ).innerHTML = `${usedPercentage}%`;
-      };
-    }
+    debounce(() => updateUI(image), 300);
   }
 
   const dpr = window.devicePixelRatio;
